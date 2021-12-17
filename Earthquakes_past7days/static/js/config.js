579e0cffa9616492e141d8793672b8307d4732ab
Juan De La Torre <juandelatorre@MacBook-Air.local>
2021-12-16 16:10:28 -0800

    pushing the last bit of the module

---
+++ Earthquakes_past7days/static/js/config.js
@@ -0,0 +1,2 @@
+const API_KEY = "pk.eyJ1IjoianVhbi1kbHQiLCJhIjoiY2t4ODBod3ZuMDEwbTJvbWhwcXI5dXhmOSJ9.0_DBHGZPe1UOvFk_-D9Y9w";
+// API key
---
+++ static/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoianVhbi1kbHQiLCJhIjoiY2t4ODBod3ZuMDEwbTJvbWhwcXI5dXhmOSJ9.0_DBHGZPe1UOvFk_-D9Y9w";
